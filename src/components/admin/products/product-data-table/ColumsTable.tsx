396624d11172e24c6c5fc97cb9da3876a5a52625
sahil-dev28 <sahilkolge28@gmail.com>
2026-03-25 19:58:53 +0530

    Changes to Product Columns Table

--- src/components/admin/products/product-data-table/ColumsTable.tsx
+++ src/components/admin/products/product-data-table/ColumsTable.tsx
@@ -2,17 +2,8 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import type { Product } from "@/types/auth.types";
 import { createColumnHelper, type CellContext } from "@tanstack/react-table";
 import { ProductActions } from "./Product-Actions";
-import { useMemo } from "react";
 
 const columnHelper = createColumnHelper<Product>();
-
-const ProductImage = ({ file }: { file: File }) => {
-  const imageUrl = useMemo(() => {
-    return file ? URL.createObjectURL(file) : "";
-  }, [file]);
-
-  return <AvatarImage src={imageUrl} />;
-};
 
 export const ProductColumns = [
   {
@@ -21,8 +12,7 @@ export const ProductColumns = [
     cell: ({ row }: CellContext<Product, unknown>) => (
       <div className="flex items-center gap-3">
         <Avatar className="rounded-sm">
-          {/* <AvatarImage src={row.original.image} /> */}
-          <ProductImage file={row.original.image} />
+          <AvatarImage src={row.original.image} />
         </Avatar>
         <div className="font-medium">{row.getValue("name")}</div>
       </div>
