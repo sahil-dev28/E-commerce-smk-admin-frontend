396a25733dad61fdc35d8119d599c1c91443483c
sahil-dev28 <sahilkolge28@gmail.com>
2026-03-31 13:36:58 +0530

    base url updated
    Signed-off-by: sahil-dev28 <sahilkolge28@gmail.com>

--- src/hooks/auth/admin/useAdminLogin.tsx
+++ src/hooks/auth/admin/useAdminLogin.tsx
@@ -1,16 +1,11 @@
-// import api from "@/api/axios-instance";
+import api from "@/api/axios-instance";
 import type { AdminLoginData } from "@/types/auth.types";
 import { useMutation } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
-import axios from "axios";
 import { toast } from "sonner";
 
 export const loginAdmin = async (data: AdminLoginData) => {
-  // const response = await api.post("/auth/admin/login", data);
-  const response = await axios.post(
-    "http://localhost:8000/api/v1/auth/admin/login",
-    data,
-  );
+  const response = await api.post("/auth/admin/login", data);
   return response.data;
 };
 
